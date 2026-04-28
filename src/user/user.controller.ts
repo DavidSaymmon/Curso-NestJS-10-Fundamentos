47a31a1d95ccb7048acd1aaf0dc6fe30ff077481
DavidSaymmon <sawewfelipe@gmail.com>
2026-04-28 13:33:59 -0300

    Sessão 3

---
+++ src/user/user.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, Get, Param, Post, Patch, Put, Delete } from '@nestjs/common';
+
+@Controller('users')
+export class UserController {
+  @Post()
+  async create(@Body() body) {
+    return { body };
+  }
+  @Get()
+  async read() {
+    return { name: 'joao' };
+  }
+  @Get(':id')
+  async readOne(@Param() id: string) {
+    return { id };
+  }
+  @Put(':id')
+  async update(@Body() body, @Param() params) {
+    return {
+      method: 'put',
+      body,
+      params,
+    };
+  }
+  @Patch(':id')
+  async updatePartial(@Body() body, @Param() params) {
+    return {
+      method: 'patch',
+      body,
+      params,
+    };
+  }
+  @Delete(':id')
+  async delete(@Param() params) {
+    return {
+        params,
+    }
+  }
+}
